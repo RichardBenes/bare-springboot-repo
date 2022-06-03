fcf0355cd04bcdba3f7653162b0fdfc1c4749319
Richard Beneš <richard.benes@monster.com>
2022-06-03 11:30:35 +0200

    Using @Value with application.yaml properties

--- src/main/java/com/richardbenes/awstutorials/controllers/MyController.java
+++ src/main/java/com/richardbenes/awstutorials/controllers/MyController.java
@@ -2,6 +2,7 @@ package com.richardbenes.awstutorials.controllers;
 
 import java.time.Instant;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,15 +15,34 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class MyController {
 
-    int number = 1;
-    Instant currentTime;
+    @Value("${custom.number}")
+    private int number;
+
+    final String bestColor;
+
+    MyController(
+        @Value("${custom.color}") String color
+    ) {
+        this.bestColor = color;
+
+        // color, as a constructor parameter will be available now,
+        // number, as an injected value will be available later
+        log.info(
+            "Initialized with custom.number {} and custom.color {}",
+            number, color);
+    }
 
     @GetMapping("/")
     ResponseEntity<?> home() {
 
-        log.info("MyController.home()");
+        log.info(
+            "MyController.home(); number {} color {}", 
+            number, bestColor);
 
         return ResponseEntity
-            .ok("Hello " + this.getNumber() + " at " + Instant.now());
+            .ok(
+                "Hello, number " + this.getNumber() 
+                + ", color " + this.getBestColor()
+                + ", at " + Instant.now());
     }    
 }
